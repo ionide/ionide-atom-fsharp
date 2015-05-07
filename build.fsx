ea744710c1d3814cc56daecf4ff9396f5bd3fbf2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-05-07 17:38:51 +0200

    Copy Release script from Paket.Atom

--- build.fsx
+++ build.fsx
@@ -9,6 +9,7 @@ open System.IO
 open Fake
 open Fake.Git
 open Fake.ProcessHelper
+open Fake.ReleaseNotesHelper
 open Fake.ZipHelper
 
 #if MONO
@@ -16,16 +17,39 @@ open Fake.ZipHelper
 #load "src/atom-bindings.fsx"
 #load "src/atom-extra.fs"
 #load "src/core.fs"
-#load "src/paket.fs"
 #load "src/main.fs"
 
 #endif
+
+// Git configuration (used for publishing documentation in gh-pages branch)
+// The profile where the project is posted
+let gitOwner = "Krzysztof-Cieslak"
+let gitHome = "https://github.com/" + gitOwner
+
+// The name of the project on GitHub
+let gitName = "FSharp.Atom"
+
+// The url for the raw files hosted
+let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/Krzysztof-Cieslak"
+
+let tempReleaseDir = "temp/release"
+
+// Read additional information from the release notes document
+let releaseNotesData = 
+    File.ReadAllLines "RELEASE_NOTES.md"
+    |> parseAllReleaseNotes
+
+let release = List.head releaseNotesData
+
+let apmTool = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) </> "atom" </> "bin" </> "apm.cmd"
 
 // --------------------------------------------------------------------------------------
 // Build the Generator project and run it
 // --------------------------------------------------------------------------------------
 
-DeleteFile "src/paket/bin/paket.exe"
+Target "Clean" (fun _ ->
+    CopyFile "src/core" "README.md"
+)
 
 Target "BuildGenerator" (fun () ->
     [ __SOURCE_DIRECTORY__ @@ "src" @@ "FSharp.Atom.Generator.fsproj" ]
@@ -42,11 +66,10 @@ Target "RunGenerator" (fun () ->
 )
 #if MONO
 #else
-Target "RunScript" (fun () -> 
-    FSharp.Atom.Generator.translateModules()  
+Target "RunScript" (fun () ->
+    FSharp.Atom.Generator.translateModules()
 )
 #endif
-
 
 // --------------------------------------------------------------------------------------
 // Generate FunScript bindings from the *.d.ts files in the 'typings' folder
@@ -107,13 +130,72 @@ Target "GenerateBindings" (fun () ->
     |> ignore
 )
 
+Target "InstallDependencies" (fun _ ->
+    let args = "install"
+    
+    let srcDir = "src/core"
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- apmTool
+            info.WorkingDirectory <- srcDir
+            info.Arguments <- args) System.TimeSpan.MaxValue
+    if result <> 0 then failwithf "Error during running apm with %s" args
+)
+
+Target "TagDevelopBranch" (fun _ ->
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.pushBranch "" "origin" "develop"
+
+    let tagName = "develop-" + release.NugetVersion
+    Branches.tag "" tagName
+    Branches.pushTag "" "origin" tagName
+)
+
+
+Target "PushToMaster" (fun _ ->
+    CleanDir tempReleaseDir
+    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "master" tempReleaseDir
+
+    Git.CommandHelper.runSimpleGitCommand tempReleaseDir "rm . -f -r" |> ignore
+    CopyRecursive "src/core" tempReleaseDir true |> tracefn "%A"
+   
+    StageAll tempReleaseDir
+    Git.Commit.Commit tempReleaseDir (sprintf "Release %s" release.NugetVersion)
+    Branches.push tempReleaseDir
+)
+
+Target "Release" (fun _ ->
+    let args = sprintf "publish %s" release.NugetVersion
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- apmTool
+            info.WorkingDirectory <- tempReleaseDir
+            info.Arguments <- args) System.TimeSpan.MaxValue
+    if result <> 0 then failwithf "Error during running apm with %s" args
+)
+
 // --------------------------------------------------------------------------------------
 // Run generator by default. Invoke 'build <Target>' to override
 // --------------------------------------------------------------------------------------
 
+Target "Default" DoNothing
+
 #if MONO
-"BuildGenerator" ==> "RunGenerator"
-RunTargetOrDefault "RunGenerator"
+"Clean"
+  ==> "BuildGenerator" 
+  ==> "RunGenerator"
+  ==> "InstallDependencies"
 #else
-RunTargetOrDefault "RunScript"
+"Clean"
+  ==> "RunScript"
+  ==> "InstallDependencies"
 #endif
+
+"InstallDependencies"
+  ==> "Default"
+  ==> "TagDevelopBranch"
+  ==> "PushToMaster"
+  ==> "Release"
+
+RunTargetOrDefault "Default"
