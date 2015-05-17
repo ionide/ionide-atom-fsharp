d83d189985921884802f71142e47eeec3d6c1dbc
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-05-17 12:13:49 +0200

    Fix #44

--- src/FSharpIDE.fs
+++ src/FSharpIDE.fs
@@ -35,9 +35,9 @@ type FSharpIDE() =
                             LanguageService.project path (fun _ -> addStatusNotification "Ready")
                 | None -> addStatusNotification "Ready (.fsproj not found)"
             if JS.isDefined p then Globals.readdir(p, System.Func<NodeJS.ErrnoException, string array, unit>(proj))
+            else addStatusNotification "Ready (.fsproj not found)"
 
-        if JS.isDefined editor && JS.isPropertyDefined editor "buffer" then
-            addStatusNotification "Loading"
+        if JS.isDefined editor && JS.isPropertyDefined editor "buffer" && unbox<obj>(editor.buffer) <> null then
             let p = editor.buffer.file.path
             if (p.Split('.') |> fun n -> n.[n.Length - 1]  = "fsproj") || ( JS.isPropertyDefined editor "getGrammar" && editor.getGrammar().name = "F#") then
                 if JS.isDefined p then
