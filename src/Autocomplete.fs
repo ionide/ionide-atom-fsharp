e5a17433a86bf930d9f844be83ac194191eef6e5
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-05-17 19:46:31 +0200

    Update autocomplete - type of completion

--- src/Autocomplete.fs
+++ src/Autocomplete.fs
@@ -26,6 +26,44 @@ module AutocompleteProvider =
         excludeLowerPriority: bool 
         getSuggestions : GetSuggestionOptions -> Atom.Promise.Promise  }
 
+
+    let getNameFromGlyph i =
+        match i with
+        | 0 ->  "Class"
+        | 18 ->  "Enum"
+        | 108 -> "Struct"
+        | 144 -> "Struct" (* value type *)
+        | 12 ->  "Delegate"
+        | 48 ->  "Interface"
+        | 84 ->  "Module" (* module *)
+        | 90 ->  "Namespace"
+        | 72 ->  "Method"
+        | 74 ->  "Method"
+        | 78 ->  "Extension Method" (* method2 ? *)
+        | 102 -> "Property"
+        | 30 ->  "Event"
+        | 42 ->  "Field" (* fieldblue ? *)
+        | 192 ->  "Field" (* fieldyellow ? *)
+        | 6 ->  "Field" (* const *)
+        | 24 ->  "Property" (* enummember *)
+        | 36 ->  "Exception" (* exception *)
+        | 54 ->  "Text-file-icon" (* TextLine *)
+        | 60 ->  "Regular-file" (* Script *)
+        | 66 ->  "Script" (* Script2 *)
+        | 98 ->  "Tip-of-the-day" (* Formula *)
+        | 112 -> "Class" (* Template *)
+        | 120 -> "Class" (* Typedef *)
+        | 126 -> "Type" (* Type *)
+        | 132 -> "Type" (* Union *)
+        | 138 -> "Field" (* Variable *)
+        | 150 -> "Class" (* Intrinsic *)
+        | 156 -> "Breakpint" (* error *)
+        | 168 -> "Misc-files" (* Misc1 *)
+        | 174 ->  "Misc-files" (* Misc2 *)
+        | 180 -> "Misc-files" (* Misc3 *) 
+        | _ -> ""
+
+
     let getSuggestion (options:GetSuggestionOptions) =
         let path = options.editor.buffer.file.path
         let row = int options.bufferPosition.row + 1
@@ -41,8 +79,12 @@ module AutocompleteProvider =
                         let pref = if prefix = "." || prefix = "=" then "" else prefix
                         if result.Kind = "completion" then
                             result.Data
-                            |> Seq.where(fun t -> t.Contains(pref))
-                            |> Seq.map(fun t -> Atom.Promise.result t pref)
+                            |> Seq.where(fun t -> t.Name.Contains(pref))
+                            |> Seq.map(fun t -> { Promise.Suggestion.text =  t.Name
+                                                  Promise.Suggestion.replacementPrefix = pref
+                                                  Promise.Suggestion.rightLabel = t.Glyph |> getNameFromGlyph
+                                                 // Promise.Suggestion.``type`` = t.Glyph |> getNameFromGlyph |> fun n -> n.ToLower().Replace(" ","")
+                                                })
                             |> Seq.toArray
                             |> Atom.Promise.resolve
                         else
--- src/Bindings.fs
+++ src/Bindings.fs
@@ -18,14 +18,18 @@ let addCommand'(name:string, context: string, func:unit -> unit) : unit = failwi
 module Promise = 
     type Promise = class end
 
+    type Suggestion = {
+        text: string
+        replacementPrefix : string
+        rightLabel : string
+      //  ``type`` : string
+    }
+
     [<JSEmitInline("new Promise(function(resolve){{0}()})")>]
     let create( cb : unit -> unit) : Promise = failwith "JS"
 
     [<JSEmitInline("resolve({0})")>]
-    let resolve (o : obj) : unit = failwith "JS"
-
-    [<JSEmitInline("{text: {0}, replacementPrefix: {1}}")>]
-    let result (t : string) (prefix : string) : obj = failwith "JS"
+    let resolve (o : Suggestion[]) : unit = failwith "JS"
 
 module JS =
     [<JSEmitInline("({1}[{0}])")>]
