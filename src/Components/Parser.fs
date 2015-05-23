a0797f8e879f1bb5cedabe24664f61ad284278e9
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-05-23 20:54:48 +0200

    Parser fix

--- src/Components/Parser.fs
+++ src/Components/Parser.fs
@@ -51,8 +51,8 @@ module Parser =
         Globals.atom.workspace.onDidChangeActivePaneItem (fun ed -> ed |> parseProjectForEditor) |> subscriptions.Add
         Globals.atom.workspace.onDidChangeActivePaneItem (fun ed ->
             h |> Option.iter(fun h' -> h'.dispose ())
-            if JS.isDefined editor && JS.isPropertyDefined editor "buffer" && unbox<obj>(editor.buffer) <> null && JS.isPropertyDefined editor.buffer "file" && unbox<obj>(editor.buffer.file) <> null then
-                h <- ( editor.buffer.onDidStopChanging(fun _ -> LanguageService.parseEditor ed) |> Some  )
+            if JS.isDefined ed && JS.isPropertyDefined ed "buffer" && unbox<obj>(ed.buffer) <> null && JS.isPropertyDefined ed.buffer "file" && unbox<obj>(ed.buffer.file) <> null then
+                h <- ( ed.buffer.onDidStopChanging(fun _ -> LanguageService.parseEditor ed) |> Some  )
         ) |> subscriptions.Add
        
     let deactivate () = 
