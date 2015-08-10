837fd61080f0de34d6156d7c84660428e8fc6700
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-08-10 19:38:28 +0200

    Go to declaration, return from declaration update

--- src/Components/FindDeclaration.fs
+++ src/Components/FindDeclaration.fs
@@ -12,18 +12,35 @@ open Atom
 [<ReflectedDefinition>]
 module FindDeclaration =
 
+    let mutable lastFile : string option = None
+    let mutable lastPosition : TextBuffer.IPoint option = None
+
     let private openDec (data : DTO.FindDeclarationResult) =
         Globals.atom.workspace._open(data.Data.File, {OpenOptions.initialColumn = (data.Data.Column - 1); OpenOptions.initialLine = (data.Data.Line - 1) })
+
+    let private goBack () =
+        lastFile |> Option.iter (fun lf ->
+        lastPosition |> Option.iter (fun lp ->
+            Globals.atom.workspace._open(lf, {OpenOptions.initialColumn = int lp.column; OpenOptions.initialLine = int lp.row })
+            |> ignore
+                 )
+            )
 
     let private handle e =
         let editor = Globals.atom.workspace.getActiveTextEditor()
         let name = editor.buffer.file.path;
         let position = editor.getCursorBufferPosition()
+        lastFile <- Some name
+        lastPosition <- Some position
+
         LanguageService.findDeclaration name (int position.row + 1) (int position.column + 1)
         Events.once Events.FindDecl openDec
 
     let activate () =
         Globals.setTimeout((fun _ -> Globals.atom.commands.add("atom-text-editor", "symbols-view:go-to-declaration", handle |> unbox<Function> ) |> ignore), 500.)
+        |> ignore
+
+        Globals.setTimeout((fun _ -> Globals.atom.commands.add("atom-text-editor", "symbols-view:return-from-declaration", goBack |> unbox<Function> ) |> ignore), 500.)
         |> ignore
 
     let deactivate () =
