05fc236f5a01e8bec833e2b5c08f02493ef3138e
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-10-23 22:26:54 +0200

    Turn off linting

--- src/Components/Errors.fs
+++ src/Components/Errors.fs
@@ -64,5 +64,5 @@ module ErrorLinterProvider =
     let create () =
         [|
           { grammarScopes = [| "source.fsharp"; "source.fsharp.fsi"; "source.fsharp.fsx"; "source.fsharp.fsl"|]; scope = "file"; lint = lint; lintOnFly = true}
-          { grammarScopes = [| "source.fsharp"; "source.fsharp.fsi"; "source.fsharp.fsx"; "source.fsharp.fsl"|]; scope = "file"; lint = lintWarning; lintOnFly = true}
+        //  { grammarScopes = [| "source.fsharp"; "source.fsharp.fsi"; "source.fsharp.fsx"; "source.fsharp.fsl"|]; scope = "file"; lint = lintWarning; lintOnFly = true}
         |]
