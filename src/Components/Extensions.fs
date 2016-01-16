015e8b15ec600614f41c97253a26d3067c5bc844
Jared Hester <jared.hester@gmail.com>
2016-01-16 13:27:45 -0500

    optimized autocomplete
    faster edit distance function
    removed unnecessary allocations in comparison

--- src/Components/Extensions.fs
+++ src/Components/Extensions.fs
@@ -1,14 +1,8 @@
 [<ReflectedDefinition; AutoOpen>]
 module Atom.FSharp.Extensions
 
-open FunScript
+open System
 open FunScript.TypeScript
-open FunScript.TypeScript.fs
-open FunScript.TypeScript.child_process
-open FunScript.TypeScript.AtomCore
-open FunScript.TypeScript.text_buffer
-
-open Atom
 open Atom.FSharp
 
 type JQuery with
@@ -16,5 +10,9 @@ type JQuery with
         jq.html()
 
 type String with
+    static member Contains (target:string) (text:string) = 
+        text.IndexOf(target, StringComparison.OrdinalIgnoreCase) >= 0
+
     static member Replace (oldsub:string) (newsub:string) (str:string) =
         str.Replace(oldsub, newsub)
+
